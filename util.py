6f5fe27caf630b867becd26ae8c160b1ab265eac
justin-vicars <justin.vicars@du.edu>
2023-01-15 16:23:52 -0800

    added lambda for action on numbers

--- util.py
+++ util.py
@@ -1,4 +1,7 @@
-def nth_power(n, power):
-    return(i**power for i in range(n))
+def nth_power(n, fn= lambda x: x**2):
+    '''
+    calculates power for number upto n
+    '''
+    return[fn(i) for i in range(n)]
 
-print(nth_power(10, 4))
+print(nth_power(10))
