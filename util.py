88f16fb4ebdba5355946807802bde40c2f660b0e
justin-vicars <justin.vicars@du.edu>
2023-01-15 16:29:00 -0800

    resolved merge conflict

--- util.py
+++ util.py
@@ -1,6 +1,9 @@
 def nth_power(n, fn= lambda x: x**2):
     '''
     calculates power for number upto n
+    args:
+    n: highest number in the list of numbers
+    power: power for numbers to raise, default power is 2
     '''
     return[fn(i) for i in range(n)]
 
